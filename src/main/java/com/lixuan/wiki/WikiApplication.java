36ffa94fc6b54e82bda38cbdc8d749c8ce620aac
860238659 <860238659@qq.com>
2023-05-25 08:52:09 +0800

    Initialization configuration.

--- src/main/java/com/lixuan/wiki/WikiApplication.java
+++ src/main/java/com/lixuan/wiki/WikiApplication.java
@@ -3,11 +3,12 @@ package com.lixuan.wiki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication 是指示Spring Boot应用程序的入口点的组合注释
 @SpringBootApplication
 public class WikiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
 
 }
